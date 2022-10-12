2d1648b0fa0db4cfa122ac780368ed0bf037e54a
gab1144 <gabry.rinciari04@gmail.com>
2022-10-12 17:20:16 +0200

    [change]layout, bottone nuovo biglietto e commenti

--- js/script.js
+++ js/script.js
@@ -1,34 +1,38 @@
+const btnGenera = document.getElementById('btn-genera');
+const reset = document.getElementById('reset');
+const btnNew = document.getElementById('new');
+const biglietto = document.querySelector('#biglietto');
+const nuovoBiglietto = document.querySelector('#nuovo-biglietto');
 const prezzoKm = 0.21;
+const scontoMinorenni = 20;
+const scontoOver65 = 40;
+let datiValidi = true;
 let kmPercorso;
 let nomeCognome;
 let prezzoIniziale;
-const scontoMinorenni = 20;
-const scontoOver65 = 40;
-const btnGenera = document.getElementById('btn-genera');
-const reset = document.getElementById('reset');
 let sconto;
 let prezzoFinale;
-let datiValidi = true;
 
-
+//scrive in tabella i valori relativi agli sconti
 document.getElementById('sconto-min').innerHTML= scontoMinorenni + "%";
 document.getElementById('sconto-over').innerHTML= scontoOver65 + "%";
 
+//al click del bottone genera legge i dati inseriti dall'utente, effettua un controllo e mostra il biglietto
 btnGenera.addEventListener('click', function(){
 
   kmPercorso = parseInt (document.getElementById('km-percorso').value);
   nomeCognome = document.getElementById('nome-cognome').value;
   prezzoIniziale= kmPercorso * 0.21;
   datiValidi = true;
-  
+
   if (kmPercorso < 1 || isNaN(kmPercorso )) {
     alert("Devi inserire un numero di chilometri maggiore di 0");
     datiValidi = false;
   }
   
   if (datiValidi) {
-    const biglietto = document.querySelector('#biglietto');
     biglietto.classList.remove('d-none');
+    nuovoBiglietto.classList.remove('d-none');
     document.getElementById('km').innerHTML= kmPercorso + " km";
     document.getElementById('eta').innerHTML= document.getElementById('fascia-eta').value;
     document.getElementById('prezzo-i').innerHTML= prezzoIniziale.toFixed(2)  + " €";
@@ -50,9 +54,20 @@ btnGenera.addEventListener('click', function(){
   }
 });
 
+//cancella tutte le informazioni inserite dall'utente nei campi di input
 reset.addEventListener('click', function(){
   document.getElementById('nome-cognome').value="";
   document.getElementById('km-percorso').value="";
   document.getElementById('fascia-eta').value="";
   
+});
+
+//cancella tutte le informazioni inserite dall'utente nei campi di input e nasconde il biglietto appena generato
+btnNew.addEventListener('click', function(){
+  document.getElementById('nome-cognome').value="";
+  document.getElementById('km-percorso').value="";
+  document.getElementById('fascia-eta').value="";
+
+  biglietto.classList.add('d-none');
+  nuovoBiglietto.classList.add('d-none');
 });
