b82cbec399466afc3288cde81e230fe484af5129
gab1144 <gabry.rinciari04@gmail.com>
2022-10-12 15:30:41 +0200

    [add]funzionamento btn-reset,genera più biglietti

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const prezzoKm = 0.21;
-const kmPercorso = parseInt (document.getElementById('km-percorso').value);
-const nomeCognome = document.getElementById('nome-cognome').value;
-const prezzoIniziale = kmPercorso * 0.21;
+let kmPercorso;
+let nomeCognome;
+let prezzoIniziale;
 const scontoMinorenni = 20;
 const scontoOver65 = 40;
 const btnGenera = document.getElementById('btn-genera');
@@ -15,18 +15,25 @@ document.getElementById('sconto-min').innerHTML= scontoMinorenni + "%";
 document.getElementById('sconto-over').innerHTML= scontoOver65 + "%";
 
 btnGenera.addEventListener('click', function(){
+
+  kmPercorso = parseInt (document.getElementById('km-percorso').value);
+  nomeCognome = document.getElementById('nome-cognome').value;
+  prezzoIniziale= kmPercorso * 0.21;
+
   if (kmPercorso < 1 ) {
     alert("Devi inserire un numero di chilometri maggiore di 0 e un'età valida (maggiore o uguale a 0)");
     datiValidi = false;
   }
   
   if (datiValidi) {
+    const biglietto = document.querySelector('#biglietto');
+    biglietto.classList.remove('d-none');
     document.getElementById('km').innerHTML= kmPercorso + " km";
     document.getElementById('eta').innerHTML= document.getElementById('fascia-eta').value;
     document.getElementById('prezzo-i').innerHTML= prezzoIniziale.toFixed(2)  + " €";
     document.getElementById('nome-passeggero').innerHTML= nomeCognome;
     
-    if (document.getElementById('fascia-eta').value === "Maggiorenne") {
+    if (document.getElementById('fascia-eta').value === "Minorenne") {
       prezzoFinale = prezzoIniziale - (prezzoIniziale * scontoMinorenni / 100);
       sconto = scontoMinorenni;
     } else if (document.getElementById('fascia-eta').value === "Over 65") {
@@ -40,4 +47,11 @@ btnGenera.addEventListener('click', function(){
     document.getElementById('sconto').innerHTML= sconto + " %";
     document.getElementById('prezzo-f').innerHTML= prezzoFinale.toFixed(2) + " €";
   }
+});
+
+reset.addEventListener('click', function(){
+  document.getElementById('nome-cognome').value="";
+  document.getElementById('km-percorso').value="";
+  document.getElementById('fascia-eta').value="";
+  
 });
